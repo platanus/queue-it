413e6bb584355db33cdb69b305067a80531c502b
Gabriel Lyon <glyon1@uc.cl>
2021-07-30 16:23:49 -0400

    test(queue-it-factories): create nodes and queues factories

--- spec/factories/queue_it/nodes.rb
+++ spec/factories/queue_it/nodes.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
-  factory :node do
-    
+  factory :node, class: 'QueueIt::Node' do
+    nodable { create(:user) }
+    queue
+
+    trait :head do
+      kind { :head }
+    end
+
+    trait :any do
+      kind { :any }
+    end
+
+    trait :tail do
+      kind { :tail }
+    end
   end
 end

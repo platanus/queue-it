28e3a264df391d2d0502a5065d406d7ec8bcd65e
Gabriel Lyon <glyon1@uc.cl>
2021-08-16 13:19:07 -0400

    test(task-factory): add with three nodes trait

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :task do
     name { "My Task" }
+
+    trait :with_three_nodes do
+      after(:create) do |task|
+        create(:queue, :with_three_nodes, queable: task)
+      end
+    end
   end
 end

26c22ed3055873c610229a6ae9564f6d80873022
Gabriel Lyon <glyon1@uc.cl>
2021-12-24 10:20:40 -0300

    feat(queue): include methods to get next node by nodable attribute

--- app/models/queue_it/queue.rb
+++ app/models/queue_it/queue.rb
@@ -27,21 +27,44 @@ module QueueIt
       size == 2
     end
 
+    def get_next_by_with_queue_length_two(nodable_attribute, attribute_value)
+      if head_node.nodable.send(nodable_attribute) == attribute_value
+        get_next_in_queue_with_length_two
+      else
+        tail_node
+      end
+    end
+
     def get_next_in_queue_with_length_two
-      next_node = ActiveRecord::Base.transaction do
+      ActiveRecord::Base.transaction do
         lock!
         old_head_node = head_node.lock!
         old_tail_node = tail_node.lock!
-        nodes.where.not(kind: :any).each { |node| node.update!(kind: :any) }
+        nodes.where.not(kind: :any).find_each { |node| node.update!(kind: :any) }
         old_head_node.update!(kind: :tail, parent_node: old_tail_node)
         old_tail_node.update!(kind: :head, parent_node: nil)
         old_head_node
       end
-      next_node
+    end
+
+    def get_next_by_in_generic_queue(nodable_attribute, attribute_value)
+      if head_node.nodable.send(nodable_attribute) == attribute_value
+        return get_next_in_queue_generic
+      end
+
+      current_node = head_node.child_node
+      while !(current_node.nodable.send(nodable_attribute) == attribute_value &&
+          current_node != tail_node)
+        current_node = current_node.child_node
+        break if current_node == tail_node
+      end
+      if current_node.nodable.send(nodable_attribute) == attribute_value
+        current_node != tail_node ? move_current_node(current_node) : tail_node
+      end
     end
 
     def get_next_in_queue_generic
-      next_node = ActiveRecord::Base.transaction do
+      ActiveRecord::Base.transaction do
         lock!
         old_head_node = head_node.lock!
         old_second_node = old_head_node.child_node.lock!
@@ -51,7 +74,6 @@ module QueueIt
         old_second_node.update!(kind: :head, parent_node: nil)
         old_head_node
       end
-      next_node
     end
 
     def push_node_when_queue_length_is_zero(nodable)
@@ -89,6 +111,21 @@ module QueueIt
         old_tail_node = tail_node.lock!
         old_tail_node.update!(kind: :any)
         nodes.create!(nodable: nodable, kind: :tail, parent_node: old_tail_node)
+      end
+    end
+
+    private
+
+    def move_current_node(current_node)
+      ActiveRecord::Base.transaction do
+        lock!
+        old_current_node = current_node.lock!
+        old_next_node = old_current_node.child_node.lock!
+        old_tail_node = tail_node.lock!
+        old_tail_node.update!(kind: :any)
+        old_current_node.update!(kind: :tail, parent_node: old_tail_node)
+        old_next_node.update!(parent_node: old_current_node.parent_node)
+        old_current_node
       end
     end
   end
